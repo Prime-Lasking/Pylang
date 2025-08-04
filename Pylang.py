c113388411c371d11d21873e31f98cb6e7da17e0
The Prime Mathematician <197593373+Prime-Lasking@users.noreply.github.com>
2025-08-05 07:31:52 +1000

    Update Pylang.py

--- Pylang.py
+++ Pylang.py
@@ -1,17 +1,21 @@
-
-def intput(x:int):
+#Useful built in functions
+def intput(x: int):
      return int(input(x))
-def strput(x:str):
+def strput(x: str):
     return str(input(x))
 with open('Pylang.txt', 'r') as file:
     content = file.read()
+# Starts errors for incorrect syntax
 if "def" in content:
     raise SyntaxError("Do you mean to use func")
 if "=" in content:
     raise SyntaxError("Do you mean to use :=")
 if 'while' in content:
     raise TypeError("No while loops,only for loops, it's Pylang")
+# Converts to Python by replacing
 content = content.replace('func','def')
 content = content.replace(':=','=')
 content = content.replace('Println','print')
+#Starts the program
 exec(content)
+
